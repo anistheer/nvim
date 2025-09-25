4a1484bdafe0551368da8b35b35b2f79d226c7db
Sergey Sporshev <SSporshev@cbgr.ru>
2025-09-25 22:12:49 +0300

    fix telescope find-file path error

--- init.lua
+++ init.lua
@@ -165,7 +165,43 @@ require('lazy').setup({
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
+      local telescope = require 'telescope'
+      local actions = require 'telescope.actions'
+
+      -- https://github.com/MagicDuck/grug-far.nvim/pull/305
+      local is_windows = vim.fn.has 'win64' == 1 or vim.fn.has 'win32' == 1
+      local vimfnameescape = vim.fn.fnameescape
+      local winfnameescape = function(path)
+        local escaped_path = vimfnameescape(path)
+        if is_windows then
+          local need_extra_esc = path:find '[%[%]`%$~]'
+          local esc = need_extra_esc and '\\\\' or '\\'
+          escaped_path = escaped_path:gsub('\\[%(%)%^&;]', esc .. '%1')
+          if need_extra_esc then
+            escaped_path = escaped_path:gsub("\\\\['` ]", '\\%1')
+          end
+        end
+        return escaped_path
+      end
+
+      local select_default = function(prompt_bufnr)
+        vim.fn.fnameescape = winfnameescape
+        local result = actions.select_default(prompt_bufnr, 'default')
+        vim.fn.fnameescape = vimfnameescape
+        return result
+      end
+
       require('telescope').setup {
+        defaults = {
+          mappings = {
+            i = {
+              ['<cr>'] = select_default,
+            },
+            n = {
+              ['<cr>'] = select_default,
+            },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
